2a7c5f47b6040afbae2a6ca9cbca24d3d4f65990
Soumya Rao <sv949@scarletmail.rutgers.edu>
2026-02-21 22:18:41 -0500

    Replace background.js

--- background.js
+++ background.js
@@ -1,7 +1,19 @@
-chrome.commands.onCommand.addListener((command) => {
-  if (command === "open-cmdai") {
-    chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
-      chrome.tabs.sendMessage(tabs[0].id, { action: "toggleCmdAI" });
-    });
+chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+  if (request.action === "askCmdAI") {
+    fetch("http://127.0.0.1:8000/ask", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(request.payload)
+    })
+      .then((res) => res.json())
+      .then((data) => sendResponse({ answer: data.answer }))
+      .catch((err) => {
+        console.error(err);
+        sendResponse({ answer: null });
+      });
+
+    return true; // Required for async response
   }
 });
